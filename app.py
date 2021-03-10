350e2a1fc81a53f2965e391a55fa63c16e75373e
hallsop <49364385+hallsop@users.noreply.github.com>
2021-03-10 14:08:24 +0000

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-	names = {1: "Mark", 2: "Liping", 3: "Jordan", 4: "Michael"}
+	names = {1: "Scott", 2: "Jim", 3: "Alex", 4: "Harvey"}
 	name = names[random.randint(1,4)]
-	return "Hello, " + name + "!"
+	return "Hello, " + name + "!!!!!!!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
